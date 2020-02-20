a74760ecc88271ba03215a39514aabd012b53f93
Anoboo <56123292+Anoboo@users.noreply.github.com>
2020-02-20 15:44:53 -0500

    Tower Defense Prototype V1.1

---
+++ NewGametest/Assets/TristianAssets/TristianScripts/Turret Scripts/Turret_LookAt_Enemy.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret_LookAt_Enemy : MonoBehaviour
+{
+    // These are for the rotation of the turret when the enemy enters the trigger
+    [SerializeField] private Transform target;
+    private Vector3 originalPos;
+    private GameObject enemy;
+    [SerializeField] private bool inTrigger = false;
+
+    // These are for the bullet
+    public GameObject laser;
+    public GameObject laserLocation;
+    private GameObject laserClone;
+    public float shootSpeed;
+    private float spawnTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(inTrigger == true)
+        {
+            // If the enemy is in the trigger and is not equal to null it will 
+            // get the transform of the enemy and have the turret rotate towards the enemy.
+            if (enemy != null)
+            {
+                target = GameObject.FindGameObjectWithTag("Enemy").transform;
+            }
+            Vector3 direction = target.position - transform.position;
+            Quaternion rotataion = Quaternion.LookRotation(direction);
+            transform.rotation = rotataion;
+
+            // This is to spawn the bullet when the turret is locked onto the enemy
+            spawnTimer -= Time.deltaTime;
+            if (spawnTimer <= 0f)
+            {
+                laserClone = Instantiate(laser, laserLocation.transform.position, transform.rotation * Quaternion.Euler(90f, 0f, 0f));
+                laserClone.GetComponent<Rigidbody>().velocity = transform.forward * shootSpeed;
+                spawnTimer = 1f;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            inTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            inTrigger = false;
+        }
+    }
+}
---
+++ NewGametest/Assets/TristianAssets/TristianScripts/Waves.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waves : MonoBehaviour
+{
+    public enum SpawnState { Spawning, Waiting, Counting };
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float spawnDelay;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public Transform[] spawnPoints;
+
+    public float timeBetweenWaves = 5f;
+    [SerializeField] private float waveCountDown;
+
+    private float searchCountdown = 1f;
+
+    public SpawnState state = SpawnState.Counting;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (spawnPoints.Length == 0)
+        {
+            Debug.LogError("No spawn points referenced.");
+        }
+        waveCountDown = timeBetweenWaves;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(state == SpawnState.Waiting)
+        {
+            if(!EnemyIsAlive())
+            {
+                WaveCompleted();
+                return;
+            }
+            else
+            {
+                return;
+            }
+        }
+        if(waveCountDown <= 0)
+        {
+            if(state != SpawnState.Spawning)
+            {
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
+        }
+        else
+        {
+            waveCountDown -= Time.deltaTime;
+        }
+    }
+
+    void WaveCompleted()
+    {
+        Debug.Log("Wave Completed!");
+
+        state = SpawnState.Counting;
+        waveCountDown = timeBetweenWaves;
+
+        if(nextWave + 1 > waves.Length - 1)
+        {
+            nextWave = 0;
+            Debug.Log("All Wave Completed! Looping...");
+        }
+        else
+        {
+            nextWave++;
+        }
+    }
+
+    bool EnemyIsAlive()
+    {
+        searchCountdown -= Time.deltaTime;
+        if(searchCountdown <= 0)
+        {
+            searchCountdown = 1f;
+            if (GameObject.FindGameObjectWithTag("Enemy") == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    IEnumerator SpawnWave(Wave _wave)
+    {
+        Debug.Log("Spawning Wave: " + _wave.name);
+        state = SpawnState.Spawning;
+
+        for(int i = 0; i < _wave.count; i++)
+        {
+            SpawnEnemy(_wave.enemy);
+            yield return new WaitForSeconds(1f / _wave.spawnDelay);
+        }
+
+        state = SpawnState.Waiting;
+        yield break;
+    }
+
+    void SpawnEnemy(Transform _enemy)
+    {
+        Debug.Log("Spawning Enemy" + _enemy.name);
+        Transform _sp = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        Instantiate(_enemy, _sp.position, transform.rotation);
+    }
+}
